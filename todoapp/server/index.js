206178a5f001a2d08491ad34a320e253a6e15cde
PruttinuShane <156614994+PruttinuShane@users.noreply.github.com>
2024-03-24 15:32:34 +0200

    todo pt5
    need to debug to fix the code

--- todoapp/server/index.js
+++ todoapp/server/index.js
@@ -5,6 +5,8 @@ const { Pool } = require('pg');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
 const port = 3001;
 
 // Create a database connection pool
@@ -35,6 +37,20 @@ app.post("/new", async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
+
+app.delete("/delete/:id", async(req,res)=> {
+    const pool = openDb()
+    const id = parseInt(req.params.id)
+    pool.query('delete from task where id =$1',
+    [id],
+    (error,result) => {
+        if (error) {
+            res.status(500).json({error: error.message})
+        } else {
+            res.status(200).json({id:id})
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
